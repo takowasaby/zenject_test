5ae4ecfc261e72306dd916e42ef5dd7c675d5580
祐介 富岡 <tomioka2012@gmail.com>
2020-04-12 18:15:39 +0900

    デモの素体を追加

---
+++ Assets/Scripts/Demo/BallBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+public class BallBehaviour : MonoBehaviour, IInitializable
+{
+    private Vector3 velocity;
+
+    public void Initialize()
+    {
+        this.velocity = new Vector3(0.1f, 0f, 0.1f);
+
+        var everyFrame = Observable.EveryFixedUpdate().Select(_ => Unit.Default);
+
+        everyFrame.Subscribe(_ => base.transform.position = base.transform.position + this.velocity);
+    }
+
+    private void Start()
+    {
+        this.Initialize();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var contactPoint = collision.contacts[0];
+        var contctNorm = contactPoint.normal.normalized;
+        this.velocity = this.velocity - 2 * Vector3.Dot(this.velocity, contctNorm) * contctNorm;
+    }
+}
---
+++ Assets/Scripts/Demo/PlayerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using Zenject;
+
+public class PlayerBehaviour : MonoBehaviour, IInitializable
+{
+    public void Initialize()
+    {
+        var everyFrame = Observable.EveryFixedUpdate().Select(_ => Unit.Default);
+
+        everyFrame
+            .Where(_ => Input.GetKey(KeyCode.RightArrow))
+            .Subscribe(_ => base.transform.position = base.transform.position + new Vector3(0.1f, 0f, 0f));
+
+        everyFrame
+            .Where(_ => Input.GetKey(KeyCode.LeftArrow))
+            .Subscribe(_ => base.transform.position = base.transform.position + new Vector3(-0.1f, 0f, 0f));
+    }
+
+    void Start()
+    {
+        this.Initialize();
+    }
+}
